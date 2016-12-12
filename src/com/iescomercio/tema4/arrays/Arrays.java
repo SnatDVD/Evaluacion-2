454bacad81ba14993bda801ed5498ea3d065b3af
Vespertino <Vespertino@C8PC08>
2016-12-12 18:13:25 +0100

    12/12/16 Arrays For

--- src/com/iescomercio/tema4/arrays/Arrays.java
+++ src/com/iescomercio/tema4/arrays/Arrays.java
@@ -2,5 +2,12 @@
 package com.iescomercio.tema4.arrays;
 
 public class Arrays {
-    int[] numeros;
+    
+    //Relizar metodo ejercicio1 que muestre la tabla del 5 hasta 5x10=50 usando un for
+    
+    public void ejercicio1(){
+        for(int i = 1; i < 11; i++){
+            System.out.println("5 x " + i + " = " + (i*5));
+        }
+    }
 }
---
+++ src/com/iescomercio/tema4/arrays/Main.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.iescomercio.tema4.arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Arrays a = new Arrays();
+        a.ejercicio1();
+    }
+}   
