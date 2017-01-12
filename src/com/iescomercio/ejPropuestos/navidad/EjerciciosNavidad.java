7a5d11ff010279edac5222de753155619cdb8eb7
Vespertino <Vespertino@C8PC08>
2017-01-12 18:04:56 +0100

    12/1/17 - Ejercicios Navidad

---
+++ src/com/iescomercio/ejPropuestos/navidad/EjerciciosNavidad.java
@@ -0,0 +1,17 @@
+
+package com.iescomercio.ejPropuestos.navidad;
+
+import java.util.Scanner;
+
+public class EjerciciosNavidad {
+    
+    public void asaltoAlCastillo(){
+        Scanner sc = new Scanner(System.in);
+        
+        
+        
+    }
+    
+    
+    
+}
