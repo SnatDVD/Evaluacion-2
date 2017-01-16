efd0a44b3bf1064bccfa8cf2063eed6fdcc5196d
Vespertino <Vespertino@C8PC08>
2017-01-16 18:51:24 +0100

    16/1/17 - Sorteo

--- src/com/iescomercio/tema5/practicaAleatorios/Main.java
+++ src/com/iescomercio/tema5/practicaAleatorios/Main.java
@@ -6,6 +6,15 @@ public class Main {
         
         Sorteo s = new Sorteo();
         
-        s.listarQuiniela(40, 30, 30);
+        //s.listarQuiniela(40, 30, 30);
+        
+        //s.primitiva();
+        
+        //s.sorteoSemanal();
+        
+        //System.out.println(s.terminacionNumFijo(4) + "%");
+        
+        System.out.println(s.terminacionNumAleatorio() + "%");
+        
     }
 }
--- src/com/iescomercio/tema5/practicaAleatorios/Sorteo.java
+++ src/com/iescomercio/tema5/practicaAleatorios/Sorteo.java
@@ -11,6 +11,27 @@ public class Sorteo {
         generador = new Random();
     }
     
+    /*
+    XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    PRIVATE
+    XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    */
+    
+    private boolean estaElNumero(int num, int[] lista){
+        for(int c = 0; c < lista.length; c++){
+            if(num == lista[c]){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    /*
+    XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    PUBLIC
+    XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    */
+    
     public void listarQuiniela(int probUno, int probDos, int probX){
         int aux;
         
@@ -31,7 +52,72 @@ public class Sorteo {
     }
     
     public void primitiva(){
+        int[] nums = new int[6];
+        int aux, c = 0;
         
+        while(c < 6){
+            aux = generador.nextInt(49)+1;
+            if(!estaElNumero(aux, nums)){
+                nums[c] = aux;
+                System.out.println("Numero " + c + ": " + nums[c]);
+                c++;
+            }
+        }
+    }
+    
+    public int sorteoNavidad(){
+        return generador.nextInt(100000);
+    }
+    
+    public void sorteoSemanal(){
+        int[] nums = new int[5];
+        
+        for(int c = 0; c < 5; c++){
+            nums[c] = generador.nextInt(10);
+            System.out.println("El " + nums[c]);
+        }
+        System.out.print("El gordo es el ");
+        for(int c = 0; c < 5; c++){
+            System.out.print(nums[c]);
+        }
+        System.out.println("");
+    }
+    
+    public int terminacionNumFijo(int num){
+        int cont = 0, cont2 = 0, aux, num2 = num%10;
+        
+        for(int x = 0; x < 100; x++){
+            for(int c = 0; c < 25; c++){
+                aux = (sorteoNavidad()%10);
+                if(num2 == aux){
+                    cont++;
+                }
+            }
+            if (cont > 0){
+                cont2++;
+            }
+            cont = 0;
+        }
+        return cont2;
+    }
+    
+    public int terminacionNumAleatorio(){
+        int cont = 0, cont2 = 0, aux, num;
+        
+        for(int x = 0; x < 100; x++){
+            for(int c = 0; c < 25; c++){
+                num = generador.nextInt(10);
+                aux = (sorteoNavidad()%10);
+                if(num == aux){
+                    cont++;
+                }
+            }
+            if (cont > 0){
+                cont2++;
+            }
+            cont = 0;
+        }
+        return cont2;
     }
     
 }
